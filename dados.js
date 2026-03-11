08ba7d9b548456651f456bb9953fbbfe6e6f489c
Kristian <91225071+KristianKirschner@users.noreply.github.com>
2026-03-11 23:44:39 +0000

    aula 1

---
+++ dados.js
@@ -0,0 +1,74 @@
+db.produtos.insertMany([
+{
+nome: "Mouse Logitech",
+categoria: "Informática",
+preco: 120,
+estoque: 50,
+avaliacoes: [4,4,5],
+disponivel: true
+},
+{
+nome: "Teclado Mecânico",
+categoria: "Informática",
+preco: 350,
+estoque: 20,
+avaliacoes: [5,5,4],
+disponivel: true
+},
+{
+nome: "Smartphone Samsung",
+categoria: "Celulares",
+preco: 2200,
+estoque: 15,
+avaliacoes: [5,4,4],
+disponivel: true
+},
+{
+nome: "Smartphone Xiaomi",
+categoria: "Celulares",
+preco: 1800,
+estoque: 0,
+avaliacoes: [4,4,4],
+disponivel: false
+},
+{
+nome: "Fone Bluetooth",
+categoria: "Acessórios",
+preco: 250,
+estoque: 30,
+avaliacoes: [4,3,4],
+disponivel: true
+},
+{
+nome: "Monitor LG",
+categoria: "Informática",
+preco: 1200,
+estoque: 8,
+avaliacoes: [5,5,5],
+disponivel: true
+},
+{
+nome: "Carregador USB-C",
+categoria: "Acessórios",
+preco: 80,
+estoque: 40,
+avaliacoes: [4,4,3],
+disponivel: true
+},
+{
+nome: "Tablet Lenovo",
+categoria: "Tablets",
+preco: 1500,
+estoque: 5,
+avaliacoes: [4,5,4],
+disponivel: true
+},
+{
+nome: "Smartwatch Xiaomi",
+categoria: "Wearables",
+preco: 900,
+estoque: 12,
+avaliacoes: [4,4,5],
+disponivel: true
+}
+])
